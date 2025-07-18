c23203a10aeb4291fa759b191be9320a093a7377
Asen Dulakshana Kavinda <s22010362@ousl.lk>
2025-07-18 19:26:04 +0530

    project structure is ready now

---
+++ template.py
@@ -0,0 +1,80 @@
+import os
+from pathlib import Path
+
+project_name = "src"
+
+list_of_files = [
+
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/configuration/mongo_db_connection.py",
+    f"{project_name}/configuration/aws_connection.py",
+    f"{project_name}/cloud_storage/__init__.py",
+    f"{project_name}/cloud_storage/aws_storage.py",
+    f"{project_name}/data_access/__init__.py",
+    f"{project_name}/data_access/project_data.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/estimator.py",
+    f"{project_name}/entity/s3_estimator.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "pyproject.toml",
+    "config/model.yaml",
+    "config/schema.yaml",
+]
+
+for filepath in list_of_files:
+
+    # convert the string filepath to a Path object 
+    filepath = Path(filepath)
+
+    # split the filepath into directory (filedir) and filename parts
+    filedir, filename = os.path.split(filepath)
+
+    # if the directory path is not empty, create directory (and parentd if needed)
+    if filedir != "": 
+        os.makedirs(filedir, exist_ok=True)
+
+    # Check if the file doesn't exist OR is empty (size 0)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        # Create the file as an empty file
+        with open(filepath, "w") as f:
+            pass
+    else:
+        # If file exists and is not empty, print a message
+        print(f"file is already present at : {filepath}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
